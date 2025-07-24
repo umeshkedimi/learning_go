996cf7bacdf2a70abbde3a3c2695f27693575e87
umeshkedimi <umesh836@gmail.com>
2025-07-24 23:20:50 +0530

    Value change using pointers

--- go_24_7_25/main.go
+++ go_24_7_25/main.go
@@ -7,4 +7,11 @@ func main() {
 	myString = "Green"
 
 	log.Println("myString is set to:", myString)
+	changeUsingPointer(&myString)
+	log.Println("After myString is now set to:", myString)
+}
+
+func changeUsingPointer(s *string) {
+	newValue := "Blue"
+	*s = newValue
 }
