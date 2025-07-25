8e9ff2f7991a408e847da8e6cc391ad42da57053
umeshkedimi <umesh836@gmail.com>
2025-07-25 17:54:59 +0530

    recivers, functions of structs

--- go_25_7_25/main.go
+++ go_25_7_25/main.go
@@ -1,25 +1,23 @@
 package main
 
 import "log"
-import "time"
+// import "time"
 
 type User struct {
 	FirstName string
-	LastName string
-	PhoneNumber string
-	Age int
-	BirthDate time.Time
+}
+
+func (f *User) GetFullName() string {
+	return f.FirstName + " " + "Kedimi"
 }
 
 func main() {
-	user := User{
-		FirstName: "Umesh",
-		LastName: "Kedimi",
-		PhoneNumber: "1234567890",
-		Age: 30,
-		BirthDate: time.Date(1993, time.January, 1, 0, 0, 0, 0, time.UTC),
-	}
-	log.Println("User created:", user)
-	log.Println(user.FirstName, user.LastName, "Phone:", user.PhoneNumber, "Age:", user.Age, "BirthDate:", user.BirthDate)
+	var user1 User
+	user1.FirstName = "Umesh"
+
+	user2 := User{ FirstName: "Sujatha" }
+
+	log.Println("User 1:", user1.GetFullName())
+	log.Println("User 2:", user2.GetFullName())
 }
 
