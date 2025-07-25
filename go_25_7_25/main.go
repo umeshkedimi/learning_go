e54926d02c0f2894e7a45883cdb58cd776004c9e
umeshkedimi <umesh836@gmail.com>
2025-07-25 12:54:10 +0530

    varialble scope

--- go_25_7_25/main.go
+++ go_25_7_25/main.go
@@ -1,11 +1,20 @@
 package main
 
+import "log"
+
+var s = "Seven"
+
 func main() {
-	a, b := saySomething("Hello")
-	println(a, b)
+	var s2 = "Eight"
+
+	log.Println("s is set to:", s)
+	log.Println("s2 is set to:", s2)
+
+	saySomething(s2)
 }
 
-func saySomething(s string) (string, string) {
-	return s, "World"
+func saySomething(s3 string) (string, string) {
+	log.Println("saySomething called with:", s)
+	return s3, "World"
 
 }
