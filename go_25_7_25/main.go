7b46a6b6ea890367cc3dab85069d47bb90b863a0
umeshkedimi <umesh836@gmail.com>
2025-07-25 13:03:54 +0530

    Added struct code

--- go_25_7_25/main.go
+++ go_25_7_25/main.go
@@ -1,20 +1,25 @@
 package main
 
 import "log"
+import "time"
 
-var s = "Seven"
+type User struct {
+	FirstName string
+	LastName string
+	PhoneNumber string
+	Age int
+	BirthDate time.Time
+}
 
 func main() {
-	var s2 = "Eight"
-
-	log.Println("s is set to:", s)
-	log.Println("s2 is set to:", s2)
-
-	saySomething(s2)
+	user := User{
+		FirstName: "Umesh",
+		LastName: "Kedimi",
+		PhoneNumber: "1234567890",
+		Age: 30,
+		BirthDate: time.Date(1993, time.January, 1, 0, 0, 0, 0, time.UTC),
+	}
+	log.Println("User created:", user)
+	log.Println(user.FirstName, user.LastName, "Phone:", user.PhoneNumber, "Age:", user.Age, "BirthDate:", user.BirthDate)
 }
 
-func saySomething(s3 string) (string, string) {
-	log.Println("saySomething called with:", s)
-	return s3, "World"
-
-}
